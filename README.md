760731cdab9dad4633d09dbcb83eabe2b1bb5fcc
ayeahgodlove <ayeahgodlove5@gmail.com>
2022-11-23 06:42:49 +0100

    feat(authorization): add code for the authentication and authorization feature

---
+++ README.md
@@ -0,0 +1,7 @@
+### Application Architecture
+
+1. Repository Layer
+
+2. Domain Layer
+
+3. 
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,13 @@ import * as dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { requiresAuth } from "express-openid-connect";
+import { expressjwt } from "express-jwt";
+import jwksRsa from "jwks-rsa";
+import bodyParser from "body-parser";
+
+import { errorHandler } from "./middlewares/error.middleware";
+import { notFoundHandler } from "./middlewares/not-found.middleware";
 
 dotenv.config();
 
@@ -23,18 +30,56 @@ const app: Express = express();
 /**
  *  App Configuration
  */
+// enable the use of request body parsing middleware
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-/**
- * Server Activation
- */
+var jwtCheck = expressjwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-78podwfm.us.auth0.com/.well-known/jwks.json",
+  }) as any,
+  audience: "http://localhost:7000",
+  issuer: "https://dev-78podwfm.us.auth0.com/",
+  algorithms: ["RS256"],
+});
 
+// app.use(jwtCheck); //donot authorize the whole application
+
+//routes
 app.get("/api/v1", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+// auth routes
+app.get("/login", requiresAuth(), async (req: Request, res: Response) => {
+  res.send({
+    title: "Secured page",
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user,
+  });
+});
+app.get("/api/v1/authorized", jwtCheck, function (req, res) {
+  res.send("Secured Resource");
+});
+
+// middleware interceptions
+app.use(errorHandler);
+app.use(notFoundHandler);
+
+/**
+ * Server Activation
+ */
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Listening on port ${PORT}`);
 });
